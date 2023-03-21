053017048afed4dfb3aa38dd0812d2c3fba1c390
Josh <16jmbz@queensu.ca>
2023-03-21 15:42:37 -0400

    Wrote Javascript for Clear and Delete Display
    Wrote javascript to test the algorithm for the clear and delete display.
    Delete is a reserved word so the variable name was changed to del.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//-------------------------------------- Variable Declaration --------------------------------------------
+
 // create a variable of type const called numbers equal to all the number buttons (querySelectorAll())
 const numbers = document.querySelectorAll(".number");
 
@@ -7,6 +9,14 @@ const operators = document.querySelectorAll(".operator");
 // create a variable of type const called display, equal to the display element (querySelector())
 const display = document.querySelector(".display");
 
+
+// create a variable of type const called del, equal to the DEL element (querySelector())
+const del= document.querySelector(".delete");
+
+// create a variable of type const called clear, equal to the AC element (querySelector())
+const clear = document.querySelector(".clear");
+
+//---------------------------------------- Event Listeners -----------------------------------------------
 // Add the event listener to each number button, when the button is clicked, update the display
 numbers.forEach(number => number.addEventListener('click', function () {
     display.innerText += this.id;
@@ -17,6 +27,15 @@ operators.forEach(operator => operator.addEventListener('click', function () {
     display.innerText += this.id;
 }))
 
+//Add the event listener to the delete button, when the button is clicked, delete the previous user input from the display
+del.addEventListener('click', function(){
+    display.innerText = display.innerText.slice(0,-1);
+})
+
+// Add the event listener to the clear button, when the button is clicked, clear the entire display
+clear.addEventListener('click', function(){
+    display.innerText = '';
+})
 
 // const buttons = document.querySelectorAll(".button");
 
