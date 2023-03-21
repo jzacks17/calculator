b29b7047c7347595fc0fd498505932d3ebce253d
Josh <16jmbz@queensu.ca>
2023-03-21 15:27:23 -0400

    Wrote Javascript to Test Display Algorithm
    Wrote javascript code in script.js to display the numbers and operators
    chosen by the user. Success!

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// create a variable of type const called numbers equal to all the number buttons (querySelectorAll())
+const numbers = document.querySelectorAll(".number");
+
+// create a variable of type const called operators equal to all the operator buttons (querySelectorAll())
+const operators = document.querySelectorAll(".operator");
+
+// create a variable of type const called display, equal to the display element (querySelector())
+const display = document.querySelector(".display");
+
+// Add the event listener to each number button, when the button is clicked, update the display
+numbers.forEach(number => number.addEventListener('click', function () {
+    display.innerText += this.id;
+}))
+
+// Add the event listener to each operator button, when the button is clicked, update the display
+operators.forEach(operator => operator.addEventListener('click', function () {
+    display.innerText += this.id;
+}))
 
 
 // const buttons = document.querySelectorAll(".button");
