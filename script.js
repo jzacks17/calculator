0143b0aaa3a1e7ede5cfbb9092d876be4f4ca69e
Josh <16jmbz@queensu.ca>
2023-03-22 10:10:16 -0400

    Wrote Javascript Code to Evaluate Input
    Wrote the javascript code to evaluate user input, displaying the output
    to the display window.

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ const clear = document.querySelector(".clear");
 //create a variable of type const called equal, set it equal to the equal element
 const equal = document.querySelector(".equals");
 
-//create a variable of type let called i, assign an initial value of 0
-let i = 0;
+//create a variable of type let called numOperations, assign an initial value of 0. This will store how many operations the user has input
+let numOperations = 0;
 
 //---------------------------------------- Event Listeners -----------------------------------------------
 
@@ -31,22 +31,28 @@ let i = 0;
 numbers.forEach(number => number.addEventListener('click', function () {
     display.innerText += this.id;
 
-    if (input[i] === undefined) {
-        input[i] = {number: '', operator: ''}
+    if (input[numOperations] === undefined) {
+        input[numOperations] = { number: '', operator: '' }
 
-        }
-    
-    input[i].number += this.id;
+    }
+
+    input[numOperations].number += this.id;
 
 }))
 
 // Add the event listener to each operator button, when the button is clicked, update the display
 operators.forEach(operator => operator.addEventListener('click', function () {
+    
+    //
+    if (input[numOperations] === undefined) {
+        return;
+    }
+    
     display.innerText += this.id;
 
-    input[i].operator = this.id;
+    input[numOperations].operator = this.id;
 
-    i++;
+    numOperations++;
 
 
 }))
@@ -72,24 +78,97 @@ equal.addEventListener('click', evaluate);
 
 function evaluate() {
 
-    let sum = 0;
+    //variable to hold the sum of all the operations 
+    let sum = null;
 
-    let currentNum = 0;
+    //variable to see if the first addition/subtraction was done
+    let firstOperation = true;
 
-    for(let j = 0; j < (input.length - 1); j++){
+    //Multiplication and division
+    for (let i = 0; i < (input.length - 1); i++) {
 
-        
-       if(input[j].operator == '+'){
-            sum += +input[j].number + +input[j+1].number
+        if (input[i].operator == '×') {
+
+            //perform multiplication, storing the product in the next number
+            input[i + 1].number = +input[i].number * +input[i + 1].number;
+
+            //set previous number to null
+            input[i].number = null;
+
         }
 
-        else if(input[j].operator == '-'){
-            sum += +input[j].number - +input[j+1].number
+        else if (input[i].operator == '÷') {
+
+            //perform division, storing the product in the next number
+            input[i + 1].number = +input[i].number / +input[i + 1].number;
+
+            //set previous number to null
+            input[i].number = null;
+
+        }
+    }
+
+    //addition and subtraction
+    for (let i = 0; i < (input.length - 1); i++) {
+
+        //skip if the current number is null (means it was already multiplied/divided)
+        if (input[i].number == null) {
+            continue;
         }
 
+        let j = i + 1
+
+        //Skip until we find the next non-null number
+        while (input[j].number == null) {
+            j++;
+        }
+
+        //if current operator is addition
+        if (input[i].operator == '+') {
+
+            if (firstOperation) {
+                //add both the current number and next number
+                sum += +input[i].number + +input[j].number
+
+                //change firstOperation variable to false
+                firstOperation = false;
+            }
+
+            else {
+                //add the next number
+                sum += +input[j].number
+
+            }
+
+        }
+
+        else if (input[i].operator == '-') {
+
+
+            if (firstOperation) {
+                //add the current number and subtract the next number
+                sum += +input[i].number - +input[j].number
+
+                //change firstOperation variable to false
+                firstOperation = false;
+            }
+
+            else {
+                //subtract the next number
+                sum -= +input[j].number
+            }
+
+        }
 
     }
 
-    display.innerText = sum;
+    //check to see if there was no addition or subtraction
+    if(firstOperation){
 
+        //if so, set the sum equal to the last value 
+        sum = input[input.length - 1].number;
+    }
+
+    //Round the sum to two decimal places and display it 
+    display.innerText = (Math.round(sum*100))/100;
 }
